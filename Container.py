ec4050a04e34a000cb48ae2a47cb295e657e0bb0
JeremyLock93 <78815263+JeremyLock93@users.noreply.github.com>
2021-10-19 17:36:31 -0400

    The basis of the container code making test code

--- Container.py
+++ Container.py
@@ -2,3 +2,29 @@
 # Using existing code that is already created and modifying it for the purposes of fitting the game engine
 # These will be the attempts made and shall be used with a test code feature.
 
+
+from item import Item
+
+class Container: 
+    """This is the class that hands collection of items"""
+
+    def __init__(self):
+        self.contents = {}
+
+    def add(self, item):
+        self.contents[item.name] = item
+
+    def remove(self, item):
+        if self.contains(item.name):
+            self.contents.remove(item)
+    
+    def moveItemto(self, item, desitnation):
+        desitnation.add(item)
+        self.remove(item)
+
+    def listContents(self):
+        pass
+
+    def contains(self, item):
+        """Purpose for this function is to see if the item is in the inventory"""
+        itemList = list(self.contents.item)
