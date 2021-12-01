a6d1d2d3bdd7b93103a47bb18db96156c58e3522
JeremyLock93 <78815263+JeremyLock93@users.noreply.github.com>
2021-12-01 14:35:52 -0500

    Create EnemyNPC.py
    Enemy File

---
+++ EnemyNPC.py
@@ -0,0 +1,35 @@
+import random
+
+class Enemy:
+  def __init__(self):
+    raise notImplementedError("Do not create the enemy objects without implmentation first")
+   
+  
+  def __str__(self)
+  return self.name
+
+  def isAlive(self):
+    return self.hp > 0
+  
+  
+  class Hyena(Enemy):
+    def __init__(self):
+      self.name = Hyena
+      self.hp = 5
+      self.damage = 5
+      self.loot = fang
+      
+      
+  class SuperComputer(Enemy):
+    def __init__(self):
+      self.name = SuperComputer
+      self.hp = 40
+      for randint(1,10):
+        if randint == 10: 
+          self.damage = 20
+        elif randint > 5 AND < 10:
+          self.damage = 10
+        else:
+          self.damage = 5
+      
+     
