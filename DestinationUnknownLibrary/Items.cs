b3f00b84416e14a40426b42001ae03b72d9685da
Jonesk6843 <70590712+Jonesk6843@users.noreply.github.com>
2022-09-25 09:33:14 -0400

    SqlDatabaseAccess Table check
    I have added the SQlDatabaseAccess for the LoadMob, LoadRoom, and LoadPlayer methods that contained tables. I am submitting this patch as a checking point to ensure all the data is in the right place and there are no problems with the table methods.
    
    **Other Points**
    -There's a password set in place on the title screen and I'm not sure how to clear it to start the program over.
    -I'm not sure if these are all the files that the text adventure needs to run, as Github is capping the project at 100 files.

--- DestinationUnknownLibrary/Items.cs
+++ DestinationUnknownLibrary/Items.cs
@@ -17,9 +17,9 @@ namespace DestinationUnknownLibrary
 	public class Items : Item
 	{
 		// Stores all created item objects
-		public static List<Items> Item = new List<Items>();
+		public static List<Items> items = new List<Items>();
 
-		public Items(int id, string name, int price, bool questItem, bool required, string description)
+        public Items(int id, string name, int price, bool questItem, bool required, string description)
 			:base(id,name,price,description)
 		{
 			Id = id;
@@ -32,26 +32,10 @@ namespace DestinationUnknownLibrary
 		public bool QuestItem { get; set; }
 		public bool Required { get; set; }
 
-
-		// Loads Items
-		public static void LoadItems()
+		// Calls the DB Query and Creates the objects from the DB records
+		public static void Load()
 		{
-			string line;
-			StreamReader reader = File.OpenText("Game_Data/Items.csv");
-			while ((line = reader.ReadLine()) != null)
-			{
-				string[] tokens = line.Split(',');
-
-				int id = int.Parse(tokens[0]);
-				string name = tokens[1];
-				int price = int.Parse(tokens[2]);
-				bool questItem = bool.Parse(tokens[3]);
-				bool required = bool.Parse(tokens[4]);
-				string description = tokens[5];
-
-				Item.Add(new Items(id, name, price, questItem, required, description));
-			}
-			reader.Close();
+			items = SqliteDataAccess.LoadItems();
 		}
-	}
+    }
 }
---
+++ DestinationUnknownLibrary/sqliteDataAccess.cs
@@ -0,0 +1,368 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+
+namespace DestinationUnknownLibrary
+{
+    public class SqliteDataAccess
+    {
+        // Does a select query to gather all records and their fields from the DB
+        //Accessing Items Table
+        public static List<Items> LoadItems()
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("SELECT * FROM Item", con);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<Items> items = new List<Items>();
+
+                while (rdr.Read())
+                {
+                    Items item = new Items(rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2), Convert.ToBoolean(rdr.GetString(3)), Convert.ToBoolean(rdr.GetString(4)), rdr.GetString(5));
+                    items.Add(item);
+                }
+                return items;
+            }
+        }
+
+        //Accessing Weapons Table
+        public static List<Weapons> LoadWeapons()
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("SELECT * FROM Weapon", con);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<Weapons> weapons = new List<Weapons>();
+
+                while (rdr.Read())
+                {
+                    Weapons weapon = new Weapons(rdr.GetInt32(0), rdr.GetString(1), rdr.GetString(2), rdr.GetString(3), rdr.GetInt32(4), rdr.GetInt32(5));
+                    weapons.Add(weapon);
+                }
+                return weapons;
+            }
+        }
+
+        //Accessing Mobs Table
+        public static List<Mobs> LoadMobs()
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("SELECT * FROM Mob", con);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<Mobs> mobs = new List<Mobs>();
+
+                while (rdr.Read())
+                {
+                    List<string> inv = LoadMobInv(Convert.ToString(rdr.GetInt32(0)));
+                    Mobs mob = new Mobs(rdr.GetInt32(0), rdr.GetString(1), rdr.GetString(2), rdr.GetInt32(3), rdr.GetInt32(4), rdr.GetString(5), rdr.GetString(6), inv);
+                    mobs.Add(mob);
+                }
+                return mobs;
+            }
+        }
+
+        //Accessing List for mobInventory
+        public static List<string> LoadMobInv(string value)
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("Select * from MobInventory WHERE MobID == @mobID", con);
+                cmd.Parameters.AddWithValue("@mobID", value);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<string> inv = new List<string>();
+                while (rdr.Read())
+                {
+                    if (!rdr.IsDBNull(2))
+                    {
+                        inv.Add(rdr.GetString(2));
+                    }
+                    else if (!rdr.IsDBNull(3))
+                    {
+                        inv.Add(rdr.GetString(3));
+                    }
+                    else if (!rdr.IsDBNull(4))
+                    {
+                        inv.Add(rdr.GetString(4));
+                    }
+                    else if (!rdr.IsDBNull(5))
+                    {
+                        inv.Add(rdr.GetString(5));
+                    }
+                }
+                return inv;
+            }
+        }
+        //Accessing Potions Table
+        public static List<Potions> LoadPotions()
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("SELECT * FROM Potion", con);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<Potions> potions = new List<Potions>();
+
+                while (rdr.Read())
+                {
+                    Potions potion = new Potions(rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2), rdr.GetInt32(3), rdr.GetString(4));
+                    potions.Add(potion);
+                }
+                return potions;
+            }
+        }
+
+        //Accessing Rooms Table
+        public static List<Rooms> LoadRooms()
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("SELECT * FROM Room", con);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<Rooms> rooms = new List<Rooms>();
+
+                while (rdr.Read())
+                {
+                    List<int> RMob = LoadRoomMob(rdr.GetInt32(0));
+                    List<int> RLoot = LoadRoomInventory(rdr.GetInt32(0));
+                    List<int> RExit = LoadRoomExit(rdr.GetInt32(0));
+                    Rooms room = new Rooms(rdr.GetInt32(0), rdr.GetString(1), rdr.GetString(2), RMob, RLoot, RExit);
+                    rooms.Add(room);
+                }
+                return rooms;
+            }
+        }
+
+        //Setting roomMob List to variable
+        public static List<int> LoadRoomMob(int value)
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("Select * from Rooms WHERE RoomID == @RoomsID", con);
+                cmd.Parameters.AddWithValue("@RoomsID", value);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<int> RMob = new List<int>();
+                while (rdr.Read())
+                {
+                    if (!rdr.IsDBNull(2))
+                    {
+                        RMob.Add(rdr.GetInt32(2));
+                    }
+                    else if (!rdr.IsDBNull(3))
+                    {
+                        RMob.Add(rdr.GetInt32(3));
+                    }
+                    else if (!rdr.IsDBNull(4))
+                    {
+                        RMob.Add(rdr.GetInt32(4));
+                    }
+                    else if (!rdr.IsDBNull(5))
+                    {
+                        RMob.Add(rdr.GetInt32(5));
+                    }
+                }
+                return RMob;
+            }
+        }
+
+        //Setting roomInventory List to variable
+        public static List<int> LoadRoomInventory(int value)
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("Select * from Rooms WHERE RoomID == @RoomsID", con);
+                cmd.Parameters.AddWithValue("@RoomsID", value);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<int> RInv = new List<int>();
+                while (rdr.Read())
+                {
+                    if (!rdr.IsDBNull(2))
+                    {
+                        RInv.Add(rdr.GetInt32(2));
+                    }
+                    else if (!rdr.IsDBNull(3))
+                    {
+                        RInv.Add(rdr.GetInt32(3));
+                    }
+                    else if (!rdr.IsDBNull(4))
+                    {
+                        RInv.Add(rdr.GetInt32(4));
+                    }
+                    else if (!rdr.IsDBNull(5))
+                    {
+                        RInv.Add(rdr.GetInt32(5));
+                    }
+                }
+                return RInv;
+            }
+        }
+
+        //Setting roomExit List to variable
+        public static List<int> LoadRoomExit(int value)
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("Select * from Rooms WHERE RoomID == @RoomsID", con);
+                cmd.Parameters.AddWithValue("@RoomsID", value);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<int> RExit = new List<int>();
+                while (rdr.Read())
+                {
+                    if (!rdr.IsDBNull(2))
+                    {
+                        RExit.Add(rdr.GetInt32(2));
+                    }
+                    else if (!rdr.IsDBNull(3))
+                    {
+                        RExit.Add(rdr.GetInt32(3));
+                    }
+                    else if (!rdr.IsDBNull(4))
+                    {
+                        RExit.Add(rdr.GetInt32(4));
+                    }
+                    else if (!rdr.IsDBNull(5))
+                    {
+                        RExit.Add(rdr.GetInt32(5));
+                    }
+                }
+                return RExit;
+            }
+        }
+        //Accessing Treasure Table
+        public static List<Treasures> LoadTreasures()
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("SELECT * FROM Treasure", con);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<Treasures> treasures = new List<Treasures>();
+
+                while (rdr.Read())
+                {
+                    Treasures treasure = new Treasures(rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2), Convert.ToBoolean(rdr.GetString(3)), rdr.GetString(4));
+                    treasures.Add(treasure);
+                }
+                return treasures;
+            }
+        }
+
+        //Access Player Table
+        public static List<Player> LoadPlayer()
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("SELECT * FROM Player", con);
+                var inv = new SQLiteCommand("Select MobID FROM MobInv", con);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                //Creating Player Object
+                List<Player> players = new List<Player>();
+
+                while (rdr.Read())
+                {
+                    List<int> PInv = playerInventory(rdr.GetInt32(0));
+                    List<string> PQue = playerQuest(Convert.ToString(rdr.GetInt32(0)));
+                    Player player = new Player(rdr.GetInt32(0), rdr.GetString(1), rdr.GetString(2), rdr.GetString(3), rdr.GetString(4), rdr.GetInt32(5), rdr.GetInt32(6), PInv, PQue);
+                    players.Add(player);
+                }
+                return players;
+            }
+        }
+
+        //Setting playerInventory List to varibale
+        public static List<int> playerInventory(int value)
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("Select * from PlayerInventory WHERE InventoryID == @inventoryID", con);
+                cmd.Parameters.AddWithValue("@inventoryID", value);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<int> PInv = new List<int>();
+                while (rdr.Read())
+                {
+                    if (!rdr.IsDBNull(2))
+                    {
+                        PInv.Add(rdr.GetInt32(2));
+                    }
+                    else if (!rdr.IsDBNull(3))
+                    {
+                        PInv.Add(rdr.GetInt32(3));
+                    }
+                    else if (!rdr.IsDBNull(4))
+                    {
+                        PInv.Add(rdr.GetInt32(4));
+                    }
+                    else if (!rdr.IsDBNull(5))
+                    {
+                        PInv.Add(rdr.GetInt32(5));
+                    }
+                }
+                return PInv;
+            }
+        }
+
+        //Setting playerQuest List to varibale
+        public static List<string> playerQuest(string value)
+        {
+            using (var con = new SQLiteConnection(LoadConnectionString()))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand("Select * from MobInventory WHERE MobID == @mobID", con);
+                cmd.Parameters.AddWithValue("@mobID", value);
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                List<string> PQue = new List<string>();
+                while (rdr.Read())
+                {
+                    if (!rdr.IsDBNull(2))
+                    {
+                        PQue.Add(rdr.GetString(2));
+                    }
+                    else if (!rdr.IsDBNull(3))
+                    {
+                        PQue.Add(rdr.GetString(3));
+                    }
+                    else if (!rdr.IsDBNull(4))
+                    {
+                        PQue.Add(rdr.GetString(4));
+                    }
+                }
+                return PQue;
+            }
+        }
+
+        // Connection string
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
