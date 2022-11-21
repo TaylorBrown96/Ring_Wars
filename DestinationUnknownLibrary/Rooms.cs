d788943457dff6d96bf74c425d49086805f784d7
Taylor Brown <78773029+TaylorBrown96@users.noreply.github.com>
2022-11-21 09:06:56 -0500

    Add files via upload

--- DestinationUnknownLibrary/Rooms.cs
+++ DestinationUnknownLibrary/Rooms.cs
@@ -5,41 +5,36 @@
 * This program is a maze/rpg text adventure game. 
 */
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 
 namespace DestinationUnknownLibrary
 {
-	public class Rooms
-	{
-		// Stores all created room objects
-		public static List<Rooms> Room = new List<Rooms>();
+    public class Rooms
+    {
+        // Stores all created room objects
+        public static List<Rooms> Room = new List<Rooms>();
 
-		// Constructor
-		public Rooms(int room_ID, string name, string description, List<LivingCreature> mobs, List<Item> loot, List<int> exits)
-		{
-			Room_ID = room_ID;
-			Name = name;
-			Description = description;
-			Mob = mobs;
-			Loot = loot;
-			Exit = exits;
-		}
+        // Constructor
+        public Rooms(int room_ID, string name, string description, List<LivingCreature> mobs, List<Item> loot, List<int> exits)
+        {
+            Room_ID = room_ID;
+            Name = name;
+            Description = description;
+            Mob = mobs;
+            Loot = loot;
+            Exit = exits;
+        }
 
-		public int Room_ID { get; set; }
-		public string Name { get; set; }
-		public string Description { get; set; }
-		public List<LivingCreature> Mob { get; set; }
-		public List<Item> Loot { get; set; }
-		public List<int> Exit { get; set; }
+        public int Room_ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<LivingCreature> Mob { get; set; }
+        public List<Item> Loot { get; set; }
+        public List<int> Exit { get; set; }
 
-		public static void Load()
-		{
-			Room = SqliteDataAccess.LoadRooms();
-		}
-	}
+        public static void Load()
+        {
+            Room = SqliteDataAccess.LoadRooms();
+        }
+    }
 }
