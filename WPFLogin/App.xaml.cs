e961eca3c7e6b6d95ce8cccf7ee2ffef92c3e32c
Taylor Brown <78773029+TaylorBrown96@users.noreply.github.com>
2022-11-21 09:06:07 -0500

    Add files via upload

---
+++ WPFLogin/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFLogin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            // Perform tasks at application exit
+            ShowWindow(GetConsoleWindow(), SW_SHOW);
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        // Show state
+        public const int SW_SHOW = 5;
+    }
+}
