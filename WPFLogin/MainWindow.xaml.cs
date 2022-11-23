601580db6fb82846f18752e37d8db0222f3d4107
Taylor Brown <78773029+TaylorBrown96@users.noreply.github.com>
2022-11-22 23:44:50 -0500

    Add files via upload

---
+++ WPFLogin/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿/**
+* 8/15/2022 to 12/10/2022
+* CSC 253
+* Group project - Taylor Brown, Kent Jones, Kalie
+* A text adventure of Ring Wars.
+*/
+
+using System.Windows;
+using RingWarsLibrary;
+
+namespace WPFLogin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Player player = null;
+
+        public MainWindow(Player user)
+        {
+            InitializeComponent();
+            player = user;
+
+            L_Username.Content = player.Name;
+        }
+
+        private void Btn_Submit_Click(object sender, RoutedEventArgs e)
+        {
+            string usrInp = PB_Password.Password.ToString();
+
+            if (usrInp == player.Password)
+            {
+                LoadGame();
+            }
+            else
+            {
+                MessageBox.Show("Sorry but that password was incorrect!", "Error!");
+            }
+        }
+
+        private void LoadGame()
+        {
+            var game = new WPFApp.MainWindow(player);
+            game.Show();
+            this.Close();
+        }
+    }
+}
