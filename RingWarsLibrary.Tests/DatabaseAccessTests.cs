7e1c92304f374a85480eaa43b14bb5fb86eb641a
KalieFTCC <90780915+KalieFTCC@users.noreply.github.com>
2022-11-27 23:10:45 -0500

    Add files via upload
    Did not get to finish this, I don't know ways to access the data correctly and need help from mr buckwell. Left in mistakes and comments

---
+++ RingWarsLibrary.Tests/DatabaseAccessTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using RingWarsLibrary;
+using Xunit;
+using Xunit.Sdk;
+
+namespace RingWarsLibrary.Tests
+{
+    public class DatabaseAccessTests
+    {
+        //int id, string name, int price, int effects, string description
+        //[Theory]
+        //[InlineData(00, "Health", 20, 20, "Heals 20")]
+        //public void PotionAccess(int id, string name, int price, int effects, string description)
+        //{
+        //   Potions healthpotion = new Potions(null);
+
+
+        //}
+
+        /*public void PotionCall() 
+        {
+            using (var mock = AutoMock.GetLoose()) 
+            {
+                mock.Mock<SqliteDataAccess>()
+                    .Setup(x => x.GetPotion<1>("select * from POTIONS"))
+                    .Returns(GetPotionSample);
+            }
+            throw new NotImplementedException();
+        }*/
+        [Fact]
+        public void TestPotionAccessCall()
+        {
+            //arrange
+            List<Potions> potions = new List<Potions>(GetPotionSample());
+            //SqliteDataAccess.GetPotion("1")
+            List<Potions> result = SqliteDataAccess.GetPotion("1");
+            string expected = "health";
+
+            Assert.Equal(expected, result);
+
+        }
+        
+        private List<Potions> GetPotionSample() 
+        {
+            List<Potions> potions = new List<Potions>
+            {
+                new Potions(1,"health", 20, 30, "Heals 30")
+                {
+                   Id = 1,
+                   Name = "Health",
+                   Price = 20,
+                   Effects = 30,
+                   Description = "Heals 30"
+                },
+                new Potions(2,"big health", 20, 40, "Heals 40")
+                {
+                   Id = 2,
+                   Name = "Big Health",
+                   Price = 20,
+                   Effects = 30,
+                   Description = "Heals 40"
+                },
+                new Potions(3,"huge health", 20, 50, "Heals 50")
+                {
+                   Id = 3,
+                   Name = "Health",
+                   Price = 20,
+                   Effects = 30,
+                   Description = "Heals 50"
+                }
+            };
+            return potions;
+        }
+    
+    }
+}
