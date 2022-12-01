aff412cdcf601b3cb91c296fd517880352c35b85
Taylor Brown <78773029+TaylorBrown96@users.noreply.github.com>
2022-11-30 22:59:32 -0500

    Add files via upload

---
+++ WinUI_Game/Program.cs
@@ -0,0 +1,26 @@
+﻿using RingWarsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinUI_Game
+{
+    static class Program
+    {
+        private static readonly Player player;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(player));
+        }
+    }
+}
